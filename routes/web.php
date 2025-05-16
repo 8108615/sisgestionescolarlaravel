7ae9779e51b96dae0872ad7e7362d566190f2765
Erick Fernando Morales Gil <moralesgilfernando@gmail.com>
2025-05-16 02:55:46 -0400

    Se agregó hasta gestiones

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Rutas para la Configuracion del Sistema
+Route::get('/admin/configuracion',[App\Http\Controllers\ConfiguracionController::class, 'index'])->name('admin.configuracion.index')->middleware('auth');
+Route::post('/admin/configuracion/create',[App\Http\Controllers\ConfiguracionController::class, 'store'])->name('admin.configuracion.store')->middleware('auth');
+
+//Rutas para las Gestiones
+Route::get('/admin/gestiones',[App\Http\Controllers\GestionController::class, 'index'])->name('admin.gestiones.index')->middleware('auth');
+Route::get('/admin/gestiones/create',[App\Http\Controllers\GestionController::class, 'create'])->name('admin.gestiones.create')->middleware('auth');
+Route::post('/admin/gestiones/create',[App\Http\Controllers\GestionController::class, 'store'])->name('admin.gestiones.store')->middleware('auth');
+Route::get('/admin/gestiones/{id}/edit',[App\Http\Controllers\GestionController::class, 'edit'])->name('admin.gestiones.edit')->middleware('auth');
+Route::put('/admin/gestiones/{id}',[App\Http\Controllers\GestionController::class, 'update'])->name('admin.gestiones.update')->middleware('auth');
+Route::delete('/admin/gestiones/{id}',[App\Http\Controllers\GestionController::class, 'destroy'])->name('admin.gestiones.destroy')->middleware('auth');
